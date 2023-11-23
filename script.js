222e62f6e1379b5229140910d558c24555630b30
mmm <proweb2008.m@gmail.com>
2023-11-23 15:30:35 +0500

    mmm

---
+++ script.js
@@ -0,0 +1,24 @@
+const birds = document.querySelector('.birds');
+clouds = document.querySelectorAll('.cloud'),
+    sun = document.querySelector('.sun');
+    // long = document.querySelector('selector');
+
+
+window.addEventListener(`scroll`, () => {
+    let y = window.scrollY
+    let x = y
+    // let x =  window.scrollX
+    // console.log(x);
+    console.log(y);
+    clouds.forEach(cloud => {
+        let speed = cloud.getAttribute('speed')
+        cloud.style.transform = `translate(${y * speed / 3}px)`
+    })
+    birds.style.transform = `translate(${x / 2}px, ${-y / 2}px)`;
+    sun.style.transform = `translate(${x / 2}px, ${-y/ 2}px)`
+
+})
+// cloud.style.transform = `translate(${x * speed}px, ${y * speed}px)`;
+// birds.style.transform = `translate(${y})`
+// console.log(birds);
+// birds.style.transform = `translate(${y}px, ${x}) `
